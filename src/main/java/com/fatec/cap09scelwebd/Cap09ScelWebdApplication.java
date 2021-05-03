f9eceba540b74625bc7ff03ebdb3b393dc530c36
Roberto Mitsunari <roberto.mitsunari@fatec.sp.gov.br>
2021-05-03 18:22:55 -0300

    Primeiro commit

---
+++ src/main/java/com/fatec/cap09scelwebd/Cap09ScelWebdApplication.java
@@ -0,0 +1,14 @@
+package com.fatec.cap09scelwebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap09ScelWebdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap09ScelWebdApplication.class, args);
+		
+	}
+
+}
